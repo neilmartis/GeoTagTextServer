74067181480cef920a6240a8f38f5cd3217c3d09
Neil Martis <neilmartis@users.noreply.github.com>
2021-04-15 19:13:35 -0400

    PI GeotageTextServer

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+app.get('/times', (req, res) => res.send(showTimes()))
+
+showTimes = () => {
+    let result = '';
+    const times = process.env.TIMES || 5;
+    for (i = 0; i < times; i++) {
+      result += i + ' ';
+    }
+    return result;
+  }
+  
